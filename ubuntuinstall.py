0041658648f1fbf07d88fdd2f2f2977f45459323
Jonathan David Livesay <jonathan.livesay@gmail.com>
2014-08-05 13:10:04 -0500

    Initial Commit

---
+++ ubuntuinstall.py
@@ -0,0 +1,26 @@
+#! /usr/bin/python3
+
+#Python script to update ubuntu and setup git and install other programs
+
+import os
+
+def main():
+
+    os.system("sudo apt-get update")
+    os.system("sudo apt-get upgrade -y")
+    os.system("sudo apt-get install git -y")
+    print("git has been installed")
+    os.system("sudo apt-get install termainator -y")
+    os.system("sudo apt-get install filezilla -y")
+    os.system("sudo apt-get install chromium-browser -y")
+    os.system("sudo apt-get install guake -y")
+    
+     
+    os.system("git config --global user.name \"Jonathan David Livesay\"")
+    os.system("git config --global user.email \"jonathan.livesay@gmail.com\"")
+    os.system("git config --global color.ui true")
+
+    print("Install Complete!!")    
+
+if __name__ == "__main__":
+    main()
---
+++ vmwareinstall.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python3
+
+#Script to install vmware on the computer must have the vmware-tools-distrib
+# folder already copied into the vmware folder.  I may be able to do something 
+# with that later to automate it. 
+
+import os
+
+def main():
+
+#    os.system("cd /home/jonl316/vmware/vmware-tools-distrib")
+    os.chdir("/home/jonl316/vmware/vmware-tools-distrib")
+    os.system("sudo ./vmware-install.pl -d")
+
+
+if __name__ == "__main__":
+    main()
